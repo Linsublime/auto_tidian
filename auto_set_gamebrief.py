1b093f656def6aa8f0d1f0baecf101945d05aa98
YuXiaoRui <yu@oddsfair.cn>
2016-03-14 16:25:03 +0800

    有裁判,伤病和其他其它提点才标志为有提点

--- auto_set_gamebrief.py
+++ auto_set_gamebrief.py
@@ -13,17 +13,19 @@ tx = db.cursor(MySQLdb.cursors.DictCursor)
 tx.execute("select * from tidian where gamebrief = '' or isnull(gamebrief)")
 result = tx.fetchall()
 for item in result:
-    gamebrief = item.get('gamebrief')
-    gamebrief = gamebrief if gamebrief != None else ''
-    for i in xrange(1, 11):
-        num = gamebrief.count('<p')
-        if num < 3:
-            content = item.get('content' + str(i))
-            if content != None:
-                gamebrief = gamebrief + content
-                num = gamebrief.count('<p')
-                if num >= 3:
-                    tx.execute("update tidian set gamebrief = %s WHERE data_id = %s", (gamebrief, item.get('data_id')))
-                    db.commit()
-                    break
+    test_key = [k for k, v in item.iteritems() if (v in (9,2,8))&('tidianType' in k)]
+    if test_key:
+        gamebrief = item.get('gamebrief')
+        gamebrief = gamebrief if gamebrief != None else ''
+        for i in xrange(1, 11):
+            num = gamebrief.count('<p')
+            if num < 3:
+                content = item.get('content' + str(i))
+                if content != None:
+                    gamebrief = gamebrief + content
+                    num = gamebrief.count('<p')
+                    if num >= 3:
+                        tx.execute("update tidian set gamebrief = %s WHERE data_id = %s", (gamebrief, item.get('data_id')))
+                        db.commit()
+                        break
 db.close()
